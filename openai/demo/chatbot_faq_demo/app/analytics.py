2bcf8ee1a644c4fec3f7d08798e749aea23d5acb
Your Name <amit>
2023-06-21 23:30:56 +0300

    encapsulate analytics to function

--- openai/demo/chatbot_faq_demo/app/analytics.py
+++ openai/demo/chatbot_faq_demo/app/analytics.py
@@ -5,45 +5,37 @@ from sklearn.decomposition import PCA
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 import ast
+import streamlit as st
 
-# Step 1: Load the CSV file and extract the embeddings
-df = pd.read_csv('data/questions_embeddings.csv')
+def analysis():
+    # Step 1: Load the CSV file and extract the embeddings
+    df = pd.read_csv('data/questions_embeddings.csv')
 
-# Convert embeddings from strings to lists of floats
-df['embedding'] = df['embedding'].apply(lambda x: ast.literal_eval(x))
+    # Convert embeddings from strings to lists of floats
+    df['embedding'] = df['embedding'].apply(lambda x: ast.literal_eval(x))
 
-# Convert the list of embeddings to a numpy array
-embeddings = np.stack(df['embedding'].values)
+    # Convert the list of embeddings to a numpy array
+    embeddings = np.stack(df['embedding'].values)
 
-# Step 2: Cluster the embeddings using KMeans
-num_clusters = 3  # Set the number of clusters
-kmeans = KMeans(n_clusters=num_clusters, random_state=0).fit(embeddings)
+    # Step 2: Cluster the embeddings using KMeans
+    num_clusters = 3  # Set the number of clusters
+    kmeans = KMeans(n_clusters=num_clusters, random_state=0).fit(embeddings)
 
-# Assign the cluster labels back to the DataFrame
-df['cluster'] = kmeans.labels_
+    # Assign the cluster labels back to the DataFrame
+    df['cluster'] = kmeans.labels_
 
-# Step 3: Reduce dimensionality for visualization
-pca = PCA(n_components=3)
-embeddings_3d = pca.fit_transform(embeddings)
+    # Step 3: Reduce dimensionality for visualization
+    pca = PCA(n_components=3)
+    embeddings_3d = pca.fit_transform(embeddings)
 
-# Create a 3D plot
-fig = plt.figure(figsize=(10, 10))
-ax = fig.add_subplot(111, projection='3d')
+    # Create a 3D plot
+    fig = plt.figure(figsize=(10, 10))
+    ax = fig.add_subplot(111, projection='3d')
 
-# Plot each cluster with a different color
-colors = ['r', 'g', 'b']  # Set the colors for the clusters
-for i in range(num_clusters):
-    cluster_i = embeddings_3d[df['cluster'] == i]
-    ax.scatter(cluster_i[:, 0], cluster_i[:, 1], cluster_i[:, 2], c=colors[i])
-    
+    # Plot each cluster with a different color
+    colors = ['r', 'g', 'b']  # Set the colors for the clusters
+    for i in range(num_clusters):
+        cluster_i = embeddings_3d[df['cluster'] == i]
+        ax.scatter(cluster_i[:, 0], cluster_i[:, 1], cluster_i[:, 2], c=colors[i])
+    return fig, df, colors, num_clusters
 
-
-# Print questions beneath the plot, colored by cluster
-print("\nClustered Questions:\n")
-for i in range(num_clusters):
-    print(f"Cluster {i+1} (color = {colors[i]}):")
-    for question in df['question'][df['cluster'] == i].values:
-        print(question)
-    print("\n")
-
-plt.show()
